7b4a33c0f27c552e2d5a85b6cd0581a1674a20e5
joel.clement <joelclems@gmail.com>
2020-01-24 16:02:41 +0100

    get repo

--- get_repository.sh
+++ get_repository.sh
@@ -4,18 +4,16 @@ then
 fi
 
 docker_name=$1
-repository_dir_path=repositories/${docker_name}
 
 . url_repositories.ini
 url_repository_var_name=url_repository_${docker_name}
 url_repository=${!url_repository_var_name}
 
-exit 1
+repository_dir_path=repositories/${docker_name}
 
-# url_repository="https://github.com/PnX-SI/UsersHub.git -b master"
 current_dir=$(pwd)
 
-if ! [ -d $dir_path ] 
+if ! [ -d $repository_dir_path ] 
 then
 
     git clone $url_repository $repository_dir_path
